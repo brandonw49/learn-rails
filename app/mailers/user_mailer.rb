32243cac7262ea5e68ae1f68a3c4fbd5110dbb6f
Brandonw49 <Brandonw49@gmail.com>
2015-03-09 16:58:51 -0400

    Further mailer tinkering

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ApplicationMailer
 
   def contact_email(contact)
     @contact = contact
-    to_address = ENV['OWNER_EMAIL']
-    mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+    #to_address = ENV['OWNER_EMAIL']
+    to_address = "brandonw49@gmail.com"
+    mail(to: to_address, from: @contact.email, :subject => "Website Contact")
   end
 end
